96e0e71d8ff4061e45a95855063b61010c3b92ec
shivam sisodia <shivamsisodia8656816@gmail.com>
2022-12-14 21:13:32 +0530

    problems and their solutions

---
+++ B_Maximum_Substring.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define vi vector<int>
+#define vll vector<long long>
+#define mii map<int, int>
+#define si set<int>
+#define sc set<char>
+
+#define f(i,s,e) for(long long int i=s;i<e;i++)
+#define cf(i,s,e) for(long long int i=s;i<=e;i++)
+#define rf(i,e,s) for(long long int i=e-1;i>=s;i--)
+#define pb push_back
+#define eb emplace_back
+
+/* UTILS */
+#define MOD 1000000007
+#define PI 3.1415926535897932384626433832795
+#define read(type) readInt<type>()
+ll min(ll a,int b) { if (a<b) return a; return b; }
+ll min(int a,ll b) { if (a<b) return a; return b; }
+ll max(ll a,int b) { if (a>b) return a; return b; }
+ll max(int a,ll b) { if (a>b) return a; return b; }
+ll gcd(ll a,ll b) { if (b==0) return a; return gcd(b, a%b); }
+ll lcm(ll a,ll b) { return a/gcd(a,b)*b; }
+string to_upper(string a) { for (int i=0;i<(int)a.size();++i) if (a[i]>='a' && a[i]<='z') a[i]-='a'-'A'; return a; }
+string to_lower(string a) { for (int i=0;i<(int)a.size();++i) if (a[i]>='A' && a[i]<='Z') a[i]+='a'-'A'; return a; }
+bool prime(ll a) { if (a==1) return 0; for (int i=2;i<=round(sqrt(a));++i) if (a%i==0) return 0; return 1; }
+/*  All Required define Pre-Processors and typedef Constants */
+typedef long int int32;
+typedef unsigned long int uint32;
+typedef long long int int64;
+typedef unsigned long long int  uint64;
+
+void solve()
+{
+    int n;
+    cin >> n;
+    string arr;
+    cin >> arr;
+    int ans = 1;
+    int count1 = 0, count2 = 0;
+    f(i, 0, n ){
+        f(j, i,n ){
+           
+           if(arr[j]=='1'){
+               count1++;
+           }
+           else{
+               count2++;
+           }
+}
+
+if(count1>0 and count2>0){
+    ans = max(ans, count1 * count2);
+}
+else if(count1==0){
+    ans = max(ans, count2 * count2);
+}
+else
+{
+    ans = max(ans, count1 * count1);
+}
+
+if(arr[i]=='1'){
+    count1--;
+}
+else{
+    count2--;
+}
+        }
+
+        cout<< ans << endl;
+}
+
+/* Main()  function */
+int main(){
+
+int tc;
+int st=1;
+cin >> tc;
+while(st<=tc){
+solve();
+ st++;
+}
+
+return 0;
+}
---
+++ D_Coprime.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/* TYPES  */
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define vi vector<int>
+#define vll vector<long long>
+#define mii map<int, int>
+#define si set<int>
+#define sc set<char>
+
+/* FUNCTIONS */
+#define f(i,s,e) for(long long int i=s;i<e;i++)
+#define cf(i,s,e) for(long long int i=s;i<=e;i++)
+#define rf(i,e,s) for(long long int i=e-1;i>=s;i--)
+#define pb push_back
+#define eb emplace_back
+
+/* PRINTS */
+template <class T>
+void print_v(vector<T> &v) { cout << "{"; for (auto x : v) cout << x << ","; cout << "\b}"; }
+
+/* UTILS */
+#define MOD 1000000007
+#define PI 3.1415926535897932384626433832795
+#define read(type) readInt<type>()
+ll min(ll a,int b) { if (a<b) return a; return b; }
+ll min(int a,ll b) { if (a<b) return a; return b; }
+ll max(ll a,int b) { if (a>b) return a; return b; }
+ll max(int a,ll b) { if (a>b) return a; return b; }
+ll gcd(ll a,ll b) { if (b==0) return a; return gcd(b, a%b); }
+ll lcm(ll a,ll b) { return a/gcd(a,b)*b; }
+string to_upper(string a) { for (int i=0;i<(int)a.size();++i) if (a[i]>='a' && a[i]<='z') a[i]-='a'-'A'; return a; }
+string to_lower(string a) { for (int i=0;i<(int)a.size();++i) if (a[i]>='A' && a[i]<='Z') a[i]+='a'-'A'; return a; }
+bool prime(ll a) { if (a==1) return 0; for (int i=2;i<=round(sqrt(a));++i) if (a%i==0) return 0; return 1; }
+void yes() { cout<<"YES\n"; }
+void no() { cout<<"NO\n"; }
+
+/*  All Required define Pre-Processors and typedef Constants */
+typedef long int int32;
+typedef unsigned long int uint32;
+typedef long long int int64;
+typedef unsigned long long int  uint64;
+typedef map<pii, int> map_type;
+
+void solve(){
+
+     vector <int> p[1001];
+f(i,1,1001){
+    f(j,1,1001){
+if(gcd(i,j)==1){
+    p[i].pb(j);
+}
+    }
+}
+
+    ll n;
+    cin >> n;
+
+    int arr[n];
+    vector<int> id[1001];
+    f(i,1,n+1){
+        cin >> arr[i];
+        id[arr[i]].pb(i);
+    }
+
+
+   ll ans = -1;
+    for(int i = 1; i <= 1000; ++i) {
+        for(int j: p[i]) {
+            if(!id[i].empty() && !id[j].empty()) {
+                ans = max(ans, id[i].back() + id[j].back());
+            }
+        }
+    }
+
+    cout << ans << endl;
+}
+
+/* Main()  function */
+int main()
+{
+    ll t;
+    cin >> t;
+    while(t--){
+  solve();
+    }
+	return 0;
+}
