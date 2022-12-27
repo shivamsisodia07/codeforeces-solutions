bcca038aa33b366556155daf440bdeb538016f34
shivam sisodia <shivamsisodia8656816@gmail.com>
2022-12-28 00:07:12 +0530

    B

--- A_Joey_Takes_Money.cpp
+++ A_Joey_Takes_Money.cpp
@@ -7,18 +7,19 @@ using namespace std;
 #define vi vector<int>
 #define vll vector<long long>
 #define mii map<int, int>
-#define si set<int>
-#define sc set<char>
+
 /* FUNCTIONS */
 #define f(i,s,e) for(long long int i=s;i<e;i++)
 #define cf(i,s,e) for(long long int i=s;i<=e;i++)
 #define rf(i,e,s) for(long long int i=e-1;i>=s;i--)
 #define pb push_back
 #define eb emplace_back
+
 /* UTILS */
 #define MOD 1000000007
 #define PI 3.1415926535897932384626433832795
 #define read(type) readInt<type>()
+
 ll min(ll a,int b) { if (a<b) return a; return b; }
 ll min(int a,ll b) { if (a<b) return a; return b; }
 ll max(ll a,int b) { if (a>b) return a; return b; }
@@ -28,22 +29,51 @@ ll lcm(ll a,ll b) { return a/gcd(a,b)*b; }
 string to_upper(string a) { for (int i=0;i<(int)a.size();++i) if (a[i]>='a' && a[i]<='z') a[i]-='a'-'A'; return a; }
 string to_lower(string a) { for (int i=0;i<(int)a.size();++i) if (a[i]>='A' && a[i]<='Z') a[i]+='a'-'A'; return a; }
 bool isprime(ll a) { if (a==1) return 0; for (int i=2;i<=round(sqrt(a));++i) if (a%i==0) return 0; return 1; }
+
 /*  All Required define Pre-Processors and typedef Constants */
 typedef long int int32;
 typedef unsigned long int uint32;
 typedef long long int int64;
 typedef unsigned long long int  uint64;
+
 void solve()
 {
+    ll n;
+    cin >> n;
+
+    vector<ll> v(n);
+
+    f(i,0,n){
+        cin >> v[i];
+    }
+    // sort(v.begin(), v.end());
+
+ll ans = 0;
+ll last = v[0];
+for (int i =1; i < n; i++)
+{
+    if (v[i] != 1) last = last * v[i];
+           
+    ans++;
+ }
+
+        ans += last;
+
+        cout << ans*2022 ;
 
 }
+
 /* Main()  function */
 int main(){
+
 ll t;
 cin>>t;
 while(t--){
 solve();
 cout<<endl;
 }
+
 return 0;
 }
+
+// use "/n","emplace_back"
